d7fd57972ca73bd42fbd03fd2f526c976805d8e9
samnart1 <originalsamnart@gmail.com>
2024-11-12 00:04:35 +0100

    its a few minutes past midnight and i am done for today. have a job of washing dishes tomorrow, totally pumped to continue with spring tomorrow morning after i wake up and before i get to work. goodnight

--- react/React-18/my-app/src/index.js
+++ react/React-18/my-app/src/index.js
@@ -9,31 +9,60 @@ const Books = [
       'https://images-na.ssl-images-amazon.com/images/I/71xXzFQz7DL._AC_UL900_SR900,600_.jpg',
     title: "You're My Little Christmas Cookie",
     author: 'Nicola Edwards',
+    id: 1,
   },
   {
     image:
-      'https://images-na.ssl-images-amazon.com/images/I/71xXzFQz7DL._AC_UL900_SR900,600_.jpg',
-    title: "You're My Little Christmas Cookie",
-    author: 'Nicola Edwards',
+      'https://images-na.ssl-images-amazon.com/images/I/81P9B8sY4AL._AC_UL900_SR900,600_.jpg',
+    title: 'How To Catch A Turkey',
+    author: 'Adam Wallace',
+    id: 2,
   },
 ]
 
 const BookList = () => {
   return (
     <section className="booklist">
-      {Books}
+      <EventExamples />
+
+      {Books.map((book) => {
+        return <Book {...book} key={book.id} />
+      })}
+    </section>
+  )
+}
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input')
+  }
+  const handleButtonClick = () => {
+    alert('handle button click')
+  }
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
     </section>
   )
 }
 
 const Book = (props) => {
-  console.log(props)
+  const { image, title, author } = props
 
   return (
     <article className="book">
-      <img src={props.image} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   )
 }
