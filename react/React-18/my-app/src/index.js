2b8e9106f26e02fbbe2f8154633741dc596cf9bd
samnart1 <originalsamnart@gmail.com>
2024-11-11 21:56:44 +0100

    working on how to render an array of objects and i am stack and also my airpod is off so i am taking a thirty minutes break to clear my mind and charge it

--- react/React-18/my-app/src/index.js
+++ react/React-18/my-app/src/index.js
@@ -3,26 +3,32 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
-const image =
-  'https://images-na.ssl-images-amazon.com/images/I/71xXzFQz7DL._AC_UL900_SR900,600_.jpg'
-const title = "You're My Little Christmas Cookie"
-const author = 'Nicola Edwards'
-
-
+const Books = [
+  {
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/71xXzFQz7DL._AC_UL900_SR900,600_.jpg',
+    title: "You're My Little Christmas Cookie",
+    author: 'Nicola Edwards',
+  },
+  {
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/71xXzFQz7DL._AC_UL900_SR900,600_.jpg',
+    title: "You're My Little Christmas Cookie",
+    author: 'Nicola Edwards',
+  },
+]
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book image = {image} title = {title} author = {author} />
-      <Book image = {image} title = {title} author = {author} />
-      <Book image = {image} title = {title} author = {author} />
+      {Books}
     </section>
   )
 }
 
 const Book = (props) => {
-  console.log(props);
-  
+  console.log(props)
+
   return (
     <article className="book">
       <img src={props.image} alt={props.title} />
