748a044898604f551337f705e40633bff189d5b2
samnart1 <originalsamnart@gmail.com>
2024-11-11 13:37:00 +0100

    rendering things onto the screen using props

--- react/React-18/my-app/src/index.js
+++ react/React-18/my-app/src/index.js
@@ -1,19 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Greeting() {
+import './index.css'
+
+const image =
+  'https://images-na.ssl-images-amazon.com/images/I/71xXzFQz7DL._AC_UL900_SR900,600_.jpg'
+const title = "You're My Little Christmas Cookie"
+const author = 'Nicola Edwards'
+
+
+
+const BookList = () => {
   return (
-    <>
-      <h2>Hello world</h2>
-      <ul>
-        <li>
-          <a href="#">hello world</a>
-        </li>
-      </ul>
-    </>
+    <section className="booklist">
+      <Book image = {image} title = {title} author = {author} />
+      <Book image = {image} title = {title} author = {author} />
+      <Book image = {image} title = {title} author = {author} />
+    </section>
+  )
+}
+
+const Book = (props) => {
+  console.log(props);
+  
+  return (
+    <article className="book">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
+    </article>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(<BookList />)
+
+// 12 - Booklist ~ Coding Addict
