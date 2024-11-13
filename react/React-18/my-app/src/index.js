425b0cf7e4d3aee7b695b285279b0abcf1ae23ea
samnart1 <originalsamnart@gmail.com>
2024-11-13 23:14:58 +0100

    working on arrow funtions inside the returns of react functions, for instance arrow function returning on an onClick btn

--- react/React-18/my-app/src/index.js
+++ react/React-18/my-app/src/index.js
@@ -33,15 +33,22 @@ const BookList = () => {
 }
 
 const EventExamples = () => {
-  const handleFormInput = () => {
+  const handleFormInput = (e) => {
+    console.log(e.target)
+    console.log(e.target.name)
+    console.log(e.target.value)
     console.log('handle form input')
   }
-  const handleButtonClick = () => {
-    alert('handle button click')
+  // const handleButtonClick = () => {
+  //   alert('handle button click')
+  // }
+  const handleFormSubmission = (e) => {
+    e.preventDefault()
+    console.log('form submitted')
   }
   return (
     <section>
-      <form>
+      <form onSubmit={handleFormSubmission}>
         <h2>Typical Form</h2>
         <input
           type="text"
@@ -50,7 +57,8 @@ const EventExamples = () => {
           style={{ margin: '1rem 0' }}
         />
       </form>
-      <button onClick={handleButtonClick}>click me</button>
+      <button onClick={(e) => console.log('herro thele')
+      }>click me</button>
     </section>
   )
 }
