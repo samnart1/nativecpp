c0728ecb5c007189a1d8fc2301834a9009741625
samnart1 <originalsamnart@gmail.com>
2024-11-14 03:53:07 +0100

    React-18 ~ finished with a simple react project about amazon best sellers website implementing just three books and the title, nothing complex nor fancy

---
+++ react/React-18/my-app/src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+  {
+    image: img1,
+    title: "The War",
+    author: 'Nicola Edwards',
+    id: 1,
+  },
+  {
+    image: img2,
+    title: 'How To Catch A Turkey',
+    author: 'Adam Wallace',
+    id: 2,
+  },
+  {
+    image: img3,
+    title: 'Hidden Pictures',
+    author: 'Adam Sandler',
+    id: 3,
+  },
+]
--- react/React-18/my-app/src/index.js
+++ react/React-18/my-app/src/index.js
@@ -1,49 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { books } from './books'
+import Book from './Book'
 
 import './index.css'
 
-const Books = [
-  {
-    image:
-      'https://images-na.ssl-images-amazon.com/images/I/71xXzFQz7DL._AC_UL900_SR900,600_.jpg',
-    title: "You're My Little Christmas Cookie",
-    author: 'Nicola Edwards',
-    id: 1,
-  },
-  {
-    image:
-      'https://images-na.ssl-images-amazon.com/images/I/81P9B8sY4AL._AC_UL900_SR900,600_.jpg',
-    title: 'How To Catch A Turkey',
-    author: 'Adam Wallace',
-    id: 2,
-  },
-]
-
 const BookList = () => {
-  const someValue = 'shakeAndBake'
-  const displayValue = () => {
-    console.log(someValue)
-  }
   return (
-    <section className="booklist">
-      {Books.map((book) => {
-        return <Book {...book} key={book.id} displayValue={displayValue} />
-      })}
-    </section>
-  )
-}
-
-const Book = (props) => {
-  const { image, title, author, displayValue } = props
-
-  return (
-    <article className="book">
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={displayValue}>click me</button>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>Amazon Best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />
+        })}
+      </section>
+    </>
   )
 }
 
