b684c2f97bd7f846d187141723b79cc4c05c6386
samnart1 <originalsamnart@gmail.com>
2024-11-18 17:38:24 +0100

    project structure on building react projects

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -7,7 +7,7 @@ import EffectExample from './02-useEffect/useEffect-example'
 import FetchData from './02-useEffect/fetchEffect'
 import MultiReturns from './03-multipleReturns/multiReturns'
 import Challenge from './03-multipleReturns/userChallenge'
-
+import Navbar from './04-projectStructure/Navbar'
 
 function App() {
   return (
@@ -18,7 +18,8 @@ function App() {
       {/* <EffectExample /> */}
       {/* <FetchData /> */}
       {/* <MultiReturns /> */}
-      <Challenge />
+      {/* <Challenge /> */}
+      <Navbar />
     </>
   )
 }
