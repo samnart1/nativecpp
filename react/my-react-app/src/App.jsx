95e33ce2ed341aa9962fb8e08a0bf43391b53226
samnart1 <originalsamnart@gmail.com>
2024-11-18 17:05:59 +0100

    userChallenge ~ how to work with conditionals and multireturns in react

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import UseObject from './01-useState/useState-object'
 import EffectExample from './02-useEffect/useEffect-example'
 import FetchData from './02-useEffect/fetchEffect'
 import MultiReturns from './03-multipleReturns/multiReturns'
+import Challenge from './03-multipleReturns/userChallenge'
+
 
 function App() {
   return (
@@ -15,7 +17,8 @@ function App() {
       {/* <UseObject /> */}
       {/* <EffectExample /> */}
       {/* <FetchData /> */}
-      <MultiReturns />
+      {/* <MultiReturns /> */}
+      <Challenge />
     </>
   )
 }
