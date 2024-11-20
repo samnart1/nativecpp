03a31cbc3039d879e869b175c0939bff0be003a9
samnart1 <originalsamnart@gmail.com>
2024-11-21 00:10:49 +0100

    i thought i was done but there is a bug and i need to find it first thing in the morning, i can't close the multi selection after opening the answers, wonder what and where the bug is hiding

--- projects/25ps/src/components/Accordion/Accordion.jsx
+++ projects/25ps/src/components/Accordion/Accordion.jsx
@@ -3,22 +3,44 @@ import data from './data'
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null)
+  const [multiSelect, setMultiSelect] = useState(false)
+  const [multiple, setMultiple] = useState([])
 
   const handleSelected = (id) => {
-    setSelected( selected === id ? null : id)
+    setSelected(selected === id ? null : id)
+  }
+
+  const handleMulti = (id) => {
+    let copyList = [...multiple]
+    const multi = copyList.indexOf(id)
+    if (multi === -1) {
+      copyList.push(id)
+    } else {
+      copyList.splice(id, 1)
+    }
+    setMultiple(copyList)
   }
 
   return (
     <div>
+      <button onClick={() => setMultiSelect(!multiSelect)}>Toggle</button>
       <div>
         {data && data.length > 0 ? (
           data.map((item) => (
             <div>
-              <div onClick={() => handleSelected(item.id)}>
+              <div
+                onClick={
+                  multiSelect
+                    ? () => handleMulti(item.id)
+                    : () => handleSelected(item.id)
+                }
+              >
                 <h3>{item.question}</h3>
                 <span>+</span>
               </div>
-              {selected === item.id ? <div>{item.answer}</div> : null}
+              {multiSelect
+                ? multiple.indexOf(item.id) !== -1 && <div>{item.answer}</div>
+                : selected === item.id && <div>{item.answer}</div>}
             </div>
           ))
         ) : (
