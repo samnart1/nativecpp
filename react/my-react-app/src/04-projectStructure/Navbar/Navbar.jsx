e5fdeff80ec50f4528644620a5bf903ebba2ad3c
samnart1 <originalsamnart@gmail.com>
2025-05-16 16:04:31 +0200

    initial commit

--- react/my-react-app/src/04-projectStructure/Navbar/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-    return (
-      <>
-        <h2>Navbar</h2>
-      </>
-    )
-  }
-  
-  export default Navbar
--- top/intro.js
+++
@@ -1,7 +0,0 @@
-const myObject = {
-    property: 'Value',
-    otherProperty: 77,
-    "obnoxious property": function() {
-        // do stuff!
-    }
-}
