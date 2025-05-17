3498b3bc95739c5311ff0550d7301436419b353c
samnart1 <originalsamnart@gmail.com>
2025-05-17 23:53:10 +0200

    cpp

---
+++ phase1/introcpp/cpp.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "something here" << endl;
+    return -1;
+}
---
+++ phase1/introcpp/io.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter two numbers: " << endl;
+    int a = 0, b = 0;
+    cin >> a >> b;
+    cout << "The sum of the two numbers are: " << a + b << endl;
+    return 0;
+}
