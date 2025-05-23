6e414f0b29417be83d0ea340f75fb3347abb9fce
samnart1 <originalsamnart@gmail.com>
2025-05-23 14:28:21 +0200

    second project underway

---
+++ phase0/2guessGame/guessGame.h
@@ -0,0 +1,6 @@
+#ifndef GUESS_GAME_H
+#define GUESS_GAME_H
+
+class Guess_Game {};
+
+#endif
---
+++ phase0/2guessGame/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "guessGame.h"
+
+using namespace std;
+
+int main() {
+
+    cout << "Guessing game coming up!" << endl;
+
+    return 0;
+}
