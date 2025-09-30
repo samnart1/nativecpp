1d6d8e7f6ec8f02a01034b19734fa14d55acf950
samnart1 <originalsamnart@gmail.com>
2025-09-30 10:50:18 +0200

    udp server

---
+++ 02_udp_server/upd_server.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[BUFFER_SIZE];
+    socklen_t client_len = sizeof(client_addr);
+
+    //SOCK_DGRAM: socket datagram: for udp socket creation.
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {}
+}
